191cf233b92a1735cfb8f8870f25b8499e9461c9
LeaValls <leandro_valls@hotmail.com>
2023-07-16 12:56:45 -0300

    actualizando la entrega

--- preEntrega1/app.js
+++ preEntrega1/app.js
@@ -1,20 +1,48 @@
 const express = require('express');
-const productsRouter = require('./routers/products');
-const cartsRouter = require('./routers/carts');
-
 const app = express();
-const port = 8080;
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
+const expressHandlebars = require('express-handelbars');
+const productsRouter = require('./routers/products');
 
-// Middleware para procesar JSON en el cuerpo de las peticiones
-app.use(express.json());
 
-// Rutas para el manejo de productos
-app.use('/api/products', productsRouter);
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + '/views');
 
-// Rutas para el manejo de carritos
-app.use('/api/carts', cartsRouter);
 
-// Iniciar el servidor
-app.listen(port, () => {
-  console.log(`Servidor Express escuchando en http://localhost:${port}`);
+app.use(express.static('public'));
+
+
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/products', productsRouter);
+
+
+io.on('connection', (socket) => {
+  console.log('Usuario conectado');
+
+
+  socket.on('addProduct', (product) => {
+ 
+    io.emit('newProduct', product);
+  });
+
+  socket.on('deleteProduct', (productId) => {
+
+
+  
+    io.emit('deleteProduct', productId);
+  });
+
+
+  socket.on('disconnect', () => {
+    console.log('Usuario desconectado');
+  });
+});
+
+const PORT = 8080;
+http.listen(PORT, () => {
+  console.log(`Servidor escuchando en el puerto ${PORT}`);
 });
